6460e8469ad58ba4028555207f264f208170ad4a
Miguel <93895318+bendItLikeBeckham@users.noreply.github.com>
2024-03-17 19:41:03 +0800

    Added comments

--- Convert.py
+++ Convert.py
@@ -1,13 +1,16 @@
+
+# Gets Sign Bit
 def check_sign(decimal):
     if decimal < 0:
         return 1
     else:
         return 0
     
+# Gets e' value
 def get_e_prime(exponent):
     return exponent + 398
 
-
+# Returns Normalized 16 digit of the decimal
 def normalize_decimal(decimal):
     dec_string = str(decimal)
     left, _, right = dec_string.partition('.')
@@ -24,10 +27,12 @@ def normalize_decimal(decimal):
 
     return ans
 
+# Removes the "0b" at the start of a binary string after it is converted
 def remove_0b(string):
     ans = string[2:]
     return ans
 
+# Get combination field
 def get_combination_field(e_prime, normalized_input):
     e_prime_binary = bin(e_prime)
     string_e_prime_binary = str(e_prime_binary)
@@ -48,6 +53,7 @@ def get_combination_field(e_prime, normalized_input):
         string_three_digits = '0' * num_zeros  + string_three_digits 
     return first_2_combi + string_three_digits
 
+#Get Exponent Continuation
 def get_exponent_field(e_prime):
     e_prime_binary = bin(e_prime)
     string_e_prime_binary = str(e_prime_binary)
@@ -82,8 +88,3 @@ print(normalized_input)
 
 print("Sign","     ","Combination Field","     ","Exponent Continuation","     ","Coefficient Continuation","     ") 
 print(sign_bit,"     ", get_combination_field(e_prime,normalized_input),"     ", get_exponent_field(e_prime))
-
-
-
-
-
