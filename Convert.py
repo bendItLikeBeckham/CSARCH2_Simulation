4f07f7813091d96a92be2071ebf6b919bac75398
Miguel <93895318+bendItLikeBeckham@users.noreply.github.com>
2024-03-17 19:25:21 +0800

    Added main python file for the logic
    Gets the sign, combination, and exponent continuation of given inputs Decimal and Exponent (base-10)

---
+++ Convert.py
@@ -0,0 +1,89 @@
+def check_sign(decimal):
+    if decimal < 0:
+        return 1
+    else:
+        return 0
+    
+def get_e_prime(exponent):
+    return exponent + 398
+
+
+def normalize_decimal(decimal):
+    dec_string = str(decimal)
+    left, _, right = dec_string.partition('.')
+    num_zeros = 16 - len(left) - len(right)
+    ans = '0' * num_zeros + left
+    if right:
+        ans += right
+    else:
+        ans += '0' * num_zeros
+
+    if ans[-1] == '0':
+         ans = ans[:-1]
+         ans = '0' + ans
+
+    return ans
+
+def remove_0b(string):
+    ans = string[2:]
+    return ans
+
+def get_combination_field(e_prime, normalized_input):
+    e_prime_binary = bin(e_prime)
+    string_e_prime_binary = str(e_prime_binary)
+    string_e_prime_binary = remove_0b(string_e_prime_binary)
+
+    if len(string_e_prime_binary) < 10:
+        num_zeros = 10 - len(string_e_prime_binary)
+        string_e_prime_binary = '0' * num_zeros  + string_e_prime_binary
+
+    first_2_combi = string_e_prime_binary[:2]
+
+    last_three_digits = bin(int(normalized_input[0]))
+    string_three_digits = str(last_three_digits)
+    string_three_digits = remove_0b(string_three_digits)
+
+    if len(string_three_digits) < 3:
+        num_zeros = 3 - len(string_three_digits)
+        string_three_digits = '0' * num_zeros  + string_three_digits 
+    return first_2_combi + string_three_digits
+
+def get_exponent_field(e_prime):
+    e_prime_binary = bin(e_prime)
+    string_e_prime_binary = str(e_prime_binary)
+    string_e_prime_binary = remove_0b(string_e_prime_binary)
+
+    if len(string_e_prime_binary) < 10:
+        num_zeros = 10 - len(string_e_prime_binary)
+        string_e_prime_binary = '0' * num_zeros  + string_e_prime_binary
+
+    last_8_exponent = string_e_prime_binary[2:]
+
+    return last_8_exponent
+
+
+
+decimal = float(input("Input Decimal: "))
+
+exponent = int(input("Input Exponent: "))
+
+e_prime = get_e_prime(exponent)
+
+sign_bit = check_sign(decimal)
+
+normalized_input = normalize_decimal(decimal)
+
+print(normalized_input)
+
+
+#print(get_combination_field(e_prime,normalized_input))
+
+
+
+print("Sign","     ","Combination Field","     ","Exponent Continuation","     ","Coefficient Continuation","     ") 
+print(sign_bit,"     ", get_combination_field(e_prime,normalized_input),"     ", get_exponent_field(e_prime))
+
+
+
+
+
